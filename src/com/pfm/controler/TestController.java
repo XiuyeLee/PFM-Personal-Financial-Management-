2d92093cccf1ce42848fbc46160e1dd0ff2a6188
xiuye <xiuye_engineer@outlook.com>
2016-10-25 11:56:02 +0800

    new

--- src/com/pfm/controler/TestController.java
+++ src/com/pfm/controler/TestController.java
@@ -1,9 +1,11 @@
 package com.pfm.controler;
 
-
 import java.util.List;
 
 import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,16 +22,16 @@ import com.xiuye.speech.Speech;
 @Controller
 public class TestController {
 
-//	@Resource
-//	private TestDao testDao;
+	// @Resource
+	// private TestDao testDao;
 
 	@Resource
 	private TestService testService;
 
-//	@Bean
-//	public Test test123() {
-//		return new Test();
-//	}
+	// @Bean
+	// public Test test123() {
+	// return new Test();
+	// }
 
 	// @RequestMapping("/test.do")
 	// @ResponseBody
@@ -66,29 +68,30 @@ public class TestController {
 
 	@RequestMapping("test2.do")
 	public String test(Test test) {
-		System.out.println("Project's Path := "+System.getProperty("web.root"));
+		System.out.println("Project's Path := " + System.getProperty("web.root"));
 		SpeechUtil.speakOnce("您需要输入信息!");
-		if(test.getName() == null){
+		if (test.getName() == null) {
 			return "test2";
 		}
 		System.out.println("uuid := " + test.getUuid() + ",name := " + test.getName());
 
-		//this.testService.save(test);
+		// this.testService.save(test);
 		int i = this.testService.saveTest(test);
-		if(i>0){
+		if (i > 0) {
 			System.out.println("inset := " + true);
-		}
-		else{
+		} else {
 			System.out.println("inset := " + false);
 		}
 		return "test2";
 	}
+
 	@RequestMapping("test3.do")
 	@ResponseBody
-	public List<Test> selectAll(){
+	public List<Test> selectAll() {
 		SpeechUtil.speakOnce("您正在下载JSON数据!");
 		return this.testService.list();
 	}
+
 	@RequestMapping("test1.do")
 	public String test1(Test test) {
 		SpeechUtil.speakOnce("您打开了test1.");
@@ -96,14 +99,52 @@ public class TestController {
 	}
 
 	@RequestMapping("deleteAllTest.do")
-	public void delete(){
+	public void delete() {
+
 		this.testService.deleteAll();
 	}
 
-//	@Resource
-//	private Test test;
-//
-//	public Test getTest() {
-//		return test;
-//	}
+	@RequestMapping("netInfo.do")
+	public void netInfo(HttpServletRequest request, HttpSession session, HttpServletResponse response) {
+		System.out.println("===========HttpServletRequest==============");
+		System.out.println("AuthType := " + request.getAuthType());
+		System.out.println("CharacterEncoding := " + request.getCharacterEncoding());
+		System.out.println("ContentLength := " + request.getContentLength());
+		System.out.println("ContentType := " + request.getContentType());
+		System.out.println("ContextPath := " + request.getContextPath());
+		System.out.println("LocalAddr := " + request.getLocalAddr());
+		System.out.println("LocalName := " + request.getLocalName());
+		System.out.println("LocalPort := " + request.getLocalPort());
+		System.out.println("Method := " + request.getMethod());
+		System.out.println("PathInfo := " + request.getPathInfo());
+		System.out.println("PathTranslated := " + request.getPathTranslated());
+		System.out.println("Protocol := " + request.getProtocol());
+		System.out.println("QueryString := " + request.getQueryString());
+		System.out.println("RemoteAddr := " + request.getRemoteAddr());
+		System.out.println("RemoteHost := " + request.getRemoteHost());
+		System.out.println("RemotePort := " + request.getRemotePort());
+		System.out.println("RemoteUser := " + request.getRemoteUser());
+		System.out.println("RequestedSessionId := " + request.getRequestedSessionId());
+		System.out.println("RequestURI := " + request.getRequestURI());
+		System.out.println("Scheme := " + request.getScheme());
+		System.out.println("ServerName := " + request.getServerName());
+		System.out.println("ServerPort := " + request.getServerPort());
+		System.out.println("ServletPath := " + request.getServletPath());
+		System.out.println("UserPrincipal := " + request.getUserPrincipal());
+		System.out.println("===========HttpSession==============");
+		System.out.println("CreationTime := " + session.getCreationTime());
+		System.out.println("Id := " + session.getId());
+		System.out.println("LastAccessedTime := " + session.getLastAccessedTime());
+		System.out.println("MaxInactiveInterval := " + session.getMaxInactiveInterval());
+		System.out.println("===========HttpServletResponse==============");
+		System.out.println("BufferSize := " + response.getBufferSize());
+		this.testService.deleteAll();
+	}
+
+	// @Resource
+	// private Test test;
+	//
+	// public Test getTest() {
+	// return test;
+	// }
 }
